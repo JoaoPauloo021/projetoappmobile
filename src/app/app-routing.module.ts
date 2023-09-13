a119fc9206dbc13f725b765d797381a9042a25a7
JoaoPauloo021 <joaopaulorb21@gmail.com>
2023-09-13 15:50:14 -0300

    Primeiro Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './home/pages/about/about.component';
+import { ContactComponent } from './home/pages/contact/contact.component';
+import { BodyComponent } from './shared/componentes/body/body.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'body', component:BodyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/componentes/header/header.component';
+import { BodyComponent } from './shared/componentes/body/body.component';
+import { FooterComponent } from './shared/componentes/footer/footer.component';
+import { AboutComponent } from './home/pages/about/about.component';
+import { ContactComponent } from './home/pages/contact/contact.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
