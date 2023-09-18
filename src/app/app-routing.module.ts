e0b8a1328e8eb891e7476a3a9e8434b835ef8c48
JoaoPauloo021 <joaopaulorb21@gmail.com>
2023-09-18 16:09:47 -0300

    jj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './home/pages/about/about.component';
 import { ContactComponent } from './home/pages/contact/contact.component';
 import { BodyComponent } from './shared/componentes/body/body.component';
-
+import { PrivacyComponent } from './shared/componentes/privacy/privacy.component';
+import { NotFoundComponent } from './home/pages/not-found/not-found.component';
 
 const routes: Routes = [
   {path:'about', component:AboutComponent},
   {path:'contact', component:ContactComponent},
   {path:'body', component:BodyComponent},
-];
+  {path:'privacy', component:PrivacyComponent},
+  {path:'not-found', component: NotFoundComponent },
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
